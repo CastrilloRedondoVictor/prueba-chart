66402b1039237d579e3ca802e0754fde8ba95a60
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2024-11-05 14:22:09 +0100

    example works

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <PlanetChart/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlanetChart from './components/PlanetChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlanetChart
   }
 }
 </script>
---
+++ src/components/PlanetChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="width: 1000px;">
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'; // Import Chart and registerables
+import planetChartData from '../planet-data.js'; // Ensure this imports correctly
+
+Chart.register(...registerables); // Register all necessary components
+
+export default {
+  name: 'PlanetChart',
+  data() {
+    return {
+      planetChartData: planetChartData // Use the chart data defined above
+    };
+  },
+  mounted() {
+    const ctx = document.getElementById('planet-chart').getContext('2d'); // Get the 2D context of the canvas
+    new Chart(ctx, this.planetChartData); // Create a new Chart instance with the data
+  }
+};
+</script>
+
+<style scoped>
+/* Add any styles you want for the chart container */
+</style>
